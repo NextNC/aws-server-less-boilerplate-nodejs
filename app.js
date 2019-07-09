7429b96df37cde3d55e9c7511f991abf9d8cfbc0
Nuno Carvalhao <nuno.carvalhao@gmail.com>
2019-07-09 17:43:05 +0100

    feat(serverless.yml): environment varibles and stage

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require('express')
 const sls = require('serverless-http')
 const app = express()
+
+// Varible key was define in secrets.json and declred in serverless.yml
 app.get('/', async (req, res, next) => {
-  res.status(200).send('Hello World!')
+  res.status(200).send('Hello World! '+process.env.KEY)
 })
 module.exports.server = sls(app)
